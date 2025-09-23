438c8f3a5d1cb550220ad19988992d1511b16238
Daniel Faustino <daniel.fsantos@protonmail.com>
2025-09-23 21:10:45 +0000

    Adequação do sistema para evitar falhas

--- senha.py
+++ senha.py
@@ -1,29 +1,35 @@
 # programa para validar senha
 class SistemaDeLogin:
     def __init__ (self):
-        self.senha_cadastrada = None
-        self.senha_confirmada = None
+        self.senha_cadastro = None
         self.senha_acesso = None
-        self.tentavivas = 0
+        self.tentavivas_cadastro = 0
+        self.tentavivas_acesso = 0
         self.msg_bloqueio = "ACESSO BLOQUEADO"
     
-    def cadastrar_senha(self):
+    def cadastrar(self):
         # programar a lógisca para cadastro
-        while (self.tentavivas < 3):
-            self.senha_cadastrada = input("Informe a senha: ")
+        while (self.tentavivas_cadastro < 3):
+            senha1 = input("Informe a senha: ")
 
-            if len(self.senha_cadastrada) >= 8 and any(digit.isdigit() for digit in self.senha_cadastrada):
-                print("Senha salva!")
-                self.tentavivas=0
-                return self.senha_cadastrada
+            if len(senha1) >= 8 and any(digit.isdigit() for digit in senha1):
+                # Confirmar senha
+                senha2 = input("Confirme a senha:\n")
+                if (senha1 == senha2):
+                    print("Senha salva!")
+                    self.senha_cadastro = senha1
+                    return self.senha_cadastro
+                else:
+                    print("As senhas devem ser identicas.")
             else:
                 print("Senha inválida!\nA senha deve ter pelo menos 8 dígitos e um número.")
-            self.tentavivas += 1
-            print(f"tentativa:{self.tentavivas}/3")
+            self.tentavivas_cadastro += 1
+            print(f"tentativa:{self.tentavivas_cadastro}/3")
         else:
             print(self.msg_bloqueio)
+            return
 
-    def validar_senha(self):
+    '''def validar_senha(self):
         # Validar senha cadastrada
         if not self.senha_cadastrada:
             return
@@ -40,29 +46,28 @@ class SistemaDeLogin:
             self.tentavivas += 1
             print(f"Tentivas: {self.tentavivas}/3")
         else:
-            print(self.msg_bloqueio)
+            print(self.msg_bloqueio)'''
 
 
-    def acesso_sistema(self):
+    def acessar(self):
         # programar a lógica de acesso ao sistema
-        if not self.senha_confirmada:
+        if not self.senha_cadastro:
             print("Nenhuma senha salva. Programa encerrado.")
             return
-        while self.tentavivas < 3:
+        while self.tentavivas_acesso < 3:
             print("ACESSO AO SISTEMA")
             self.senha_acesso = input("SENHA: ")
 
-            if (self.senha_acesso == self.senha_cadastrada):
+            if (self.senha_acesso == self.senha_cadastro):
                 print("ACESSO LIBERADO")
                 break
             else:
                 print("Senha inválida.")
-            self.tentavivas += 1
-            print(f"Tentativa: {self.tentavivas}/3")
+            self.tentavivas_acesso += 1
+            print(f"Tentativa: {self.tentavivas_acesso}/3")
         else:
             print("PROGRAMA ENCERRADO")
 
 sistema = SistemaDeLogin()
-sistema.cadastrar_senha()
-sistema.validar_senha()
-sistema.acesso_sistema()
+sistema.cadastrar()
+sistema.acessar()
